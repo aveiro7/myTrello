3b7f9a99aceda7d639637d11cc22bf1e32d62b9e
ola <o.rosik.rosinska@gmail.com>
2014-08-13 23:39:16 +0200

    adding colours of tasks to database

--- database.sql
+++ database.sql
@@ -3,9 +3,14 @@ CREATE TABLE type (
 	name varchar DEFAULT 'todo'
 );
 
+CREATE TABLE colour (
+	id serial primary key,
+	code varchar not null
+);
 
 CREATE TABLE task (
 	id serial primary key,
 	value varchar DEFAULT '',
-	type integer REFERENCES type(id)
+	type integer REFERENCES type(id),
+	colour integer REFERENCES colour(id)
 );
