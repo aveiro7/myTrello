3759879f617faefd8023c29da26ae2c08fef904a
ola <o.rosik.rosinska@gmail.com>
2014-10-21 11:49:18 +0200

    patch for sidebar

--- myTrello.js
+++ myTrello.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
 	$('.subcategory').hide();
 
 	$('.category').click(function() {
-		$('.subcategory').hide();
-		$(this).children().toggle();
+		if (!!$(this).children().attr("class")) {
+			if ($(this).children().attr("class").indexOf("shown") >= 0) {
+				$(this).children().removeClass("shown").hide();
+			}
+			else {
+				$('.subcategory').hide();
+				$(this).children().addClass("shown");
+				$(this).children().show();
+			}
+		}
 	});
 });
